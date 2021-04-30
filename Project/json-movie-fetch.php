c118cc175eec55a43092da6268ac72ed1047c689
Abhinav1405 <78816559+Abhinav1405@users.noreply.github.com>
2021-04-30 22:11:06 +0530

    Add files via upload

---
+++ Project/json-movie-fetch.php
@@ -0,0 +1,11 @@
+<?php
+include_once("msql-connection.php");
+$ary=array();
+$query="select * from movies";
+$table=mysqli_query($dbcon,$query);
+while($row=mysqli_fetch_array($table))
+{
+    $ary[]=$row;
+}
+echo json_encode($ary);
+?>

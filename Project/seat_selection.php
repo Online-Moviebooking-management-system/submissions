439d34698b07a334153695ca0623ba5beaa26cb2
Pawan1901Puneet <56738963+Pawan1901Puneet@users.noreply.github.com>
2021-05-03 22:50:10 +0530

    Add files via upload

---
+++ Project/seat_selection.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <script src="js/jquery-1.8.2.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/seat_selection.css">
+    <script src="js/seat_selection.js"></script>
+    
+</head>
+
+<body>
+    <div id="postHolder">
+        
+    </div>
+</body>
+
+</html>

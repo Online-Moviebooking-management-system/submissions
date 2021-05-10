5fdccf04caae4e6af484c6c306cc53bb307a9cbb
Manavganjoo <56726775+Manavganjoo@users.noreply.github.com>
2021-05-10 19:46:18 +0530

    Add files via upload

---
+++ Project/js/show_time.js
@@ -0,0 +1,32 @@
+var util = {
+            qs(sel, ctx) {
+                return (ctx || document).querySelector(sel);
+            },
+            qsa(sel, ctx) {
+                return Array.from((ctx || document).querySelectorAll(sel));
+            }
+        };
+
+        class DateCarousel {
+            constructor(el) {
+                this.element = el;
+                this.prevButton = util.qs(".date-carousel-prev", el);
+                this.input = util.qs(".date-carousel-input", el);
+                this.nextButton = util.qs(".date-carousel-next", el);
+                this.input.valueAsDate = new Date();
+                this.prevButton.addEventListener("click", this.prev.bind(this));
+                this.nextButton.addEventListener("click", this.next.bind(this));
+            }
+
+            prev() {
+                this.input.stepDown();
+            }
+
+            next() {
+                this.input.stepUp();
+            }
+        }
+
+        util.qsa('.date-carousel').forEach(function(el) {
+            new DateCarousel(el)
+        });

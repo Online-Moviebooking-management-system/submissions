784a7651ad94bc58455a2c6e4dc11a39ab385684
Abhinav1405 <78816559+Abhinav1405@users.noreply.github.com>
2021-05-10 20:05:06 +0530

    Update seat_selection.js

--- Project/js/seat_selection.js
+++ Project/js/seat_selection.js
@@ -1,76 +1,98 @@
-$(document).ready(function() {
-                
-                var postHolder = $("#postHolder");
-                var tickets = [];
-            
-                var seatsArray = new Array(20);
-                for (var i = 0; i < seatsArray.length; i++)
-                    seatsArray[i] = false;
+$(document).ready(function () {
+
+    var postHolder = $("#postHolder");
+    var tickets = [];
+
+    var seatsArray = new Array(20);
+    for (var i = 0; i < seatsArray.length; i++)
+        seatsArray[i] = false;
 
 
-                var screen = 1;
-                var date = "2021-05-04";
-                var time = "13:00";
+    var screen = parseInt($("#screen_Id").val());
+    var date = $("#date").val();
+    var time = $("#time").val();
+    
 
 
-                $.getJSON("get_seats.php?screen=" + screen + "&date=" + date + "&time=" + time, function(array) {
-                        for (var i = 0; i < array.length; i++) {
-                            //alert(array[i].seats);
-                            var arry = array[i].seats.split(',');
-                            for (var j = 0; j < arry.length; j++)
-                                seatsArray[parseInt(arry[j])] = true;
-                        }
+
+    $.getJSON("get_seats.php?screen=" +screen + "&date=" + date + "&time=" + time, function (array) {
+        for (var i = 0; i < array.length; i++) {
+            //alert(array[i].seats);
+            var arry = array[i].seats.split(',');
+            for (var j = 0; j < arry.length; j++)
+                seatsArray[parseInt(arry[j])] = true;
+        }
 
 
-                        var seats = '';
-                        for (var i = 0; i < seatsArray.length; i++) {
-                            seats = seats + `<div class='seat ${seatsArray[i] ? 'seat-occupied' : 'seat-vacant'}' id=${i} data-seatid='${i}'></div>`;
+        var seats = '';
+        for (var i = 0; i < seatsArray.length; i++) {
+            seats = seats + `<div class='seat ${seatsArray[i] ? 'seat-occupied' : 'seat-vacant'}' id=${i} data-seatid='${i}'></div>`;
 
-                        }
-//innerHTML end k lie bcha k rkha hai... marks kte to kte, navabi na ghte
-                        postHolder.html(
-                            `
-        <div class='ti'>
-                 <h3>SCREEN</h3>
+        }
+        //innerHTML end k lie bcha k rkha hai... marks kte to kte, navabi na ghte
+        postHolder.html(
+            `
+        <div class='ti' style='background-image:url(uploads/screen3.jfif);'>
              </div>
-        <div class='seat-container'>
+  
+        <div class='seat-container container'>
             ${seats}
-        <button type="button" class="btn btn-outline-danger">EXIT</button>
             </div>
-        <div class='btnn'>
-        <button type="button" class="btn btn-success" id="btnClick">Book Tickets</button>
-         </div>
          `);
-                        
-                        $("#btnClick").click(function(){
-                            alert("Clicked");
-                            alert("Booking Completed!");
-                            //postTickets(tickets, t_id, sid);
-                        });
-                    
-                       
-                        var seatElement = [];
-                        for (var i = 0; i < seatsArray.length; i++) {
-                            seatElement.push(document.getElementById(i));
-                            seatElement[i].addEventListener('click', (evt) => {
 
-                                let id = evt.target.id;
-                                if (!seatsArray[id]) {
-                                    if (tickets.includes(id)) {
-                                        // Deselect
-                                        tickets = tickets.filter(i => i != id);
-                                        seatElement[id].classList.remove('seat-hold');
-                                    } else {
-                                        // Select
-                                        tickets.push(id);
-                                        seatElement[id].classList.add('seat-hold');
-                                    }
-                                    alert(tickets);
-                                }
-
-                            });
-                        }
-                    
-
-                });
+        $("#btnClick").click(function () {
+            alert("Clicked");
+            alert("Booking Completed!");
+            //postTickets(tickets, t_id, sid);
         });
+
+
+        var seatElement = [];
+        for (var i = 0; i < seatsArray.length; i++) {
+            seatElement.push(document.getElementById(i));
+            seatElement[i].addEventListener('click', (evt) => {
+
+                let id = evt.target.id;
+                if (!seatsArray[id]) 
+                {
+                        if (tickets.includes(id)) {
+                        // Deselect
+                        tickets = tickets.filter(i => i != id);
+                        seatElement[id].classList.remove('seat-hold');
+                    } else
+                        if(tickets.length == parseInt($("#seats").val()))
+                        alert("You can't book more than "+$("#seats").val()+" tickets!");
+                    else
+                    {
+                        // Select
+                        tickets.push(id);
+                        seatElement[id].classList.add('seat-hold');
+                    }
+                    //alert(tickets);
+                }
+
+            });
+        }
+
+
+    });
+    
+    //--------------------
+    $("#cancel").click(function(){
+        var movie = $("#movie").val();
+        window.location = "show_time.php?movie="+movie;
+    });
+    //-----------------------------------
+    
+    $("#book").click(function(){
+        
+        if(tickets.length < parseInt($("#seats").val()))
+            alert("Kindly Select "+$("#seats").val()+" tickets!");
+        else
+        {
+            window.location="checkout.php?screen_Id="+screen+"&date="+date+"&time="+time+"&tickets="+tickets.join();
+                
+        }
+        
+    });
+});
